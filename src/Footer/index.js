b40bd5b1e9b45a7690fbd91a80e9096e70d563a3
Rafael Douglas Pereira Leite Silva <rafael.silva@lello.com.br>
2020-06-01 19:24:51 -0300

    ajustes finais

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import logoInstagram from '../images/instagram.png';
+import logoFacebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
+import youtube from "../images/youtube.png";
+
+
 
 const Footer = () => {
   return (
@@ -9,6 +14,17 @@ const Footer = () => {
           <div class="row">
             <div class="col-md-12 text-center heading-section heading-section-white">
                <h2>Nos siga nas redes sociais</h2>
+
+                <div>
+                   <img src={logoFacebook} width="50" height="50" />
+
+                   <img src={logoInstagram} width="50" height="50" />
+
+                   <img src={twitter} width="50" height="50" />
+
+                   <img src={youtube} width="50" height="50" />
+                </div>
+
             </div>
           </div>
         </div>
